5e447b08c5289d0853b4e651d57ae3d3322413e1
Yanjie Zhou <z5174108@ad.unsw.edu.au>
2023-11-12 11:57:17 +1100

    v0.0.4 Build 111211 implement Dashboard Listing items actions

--- frontend/src/components/ItemPropertyHosted/index.jsx
+++ frontend/src/components/ItemPropertyHosted/index.jsx
@@ -1,20 +1,100 @@
 // this component is for listing item display on homepage
 
-import React from 'react';
+import { React, useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import { useNavigate as navigate } from 'react-router-dom';
+
+import { listPublish, listUnpublish, listDelete } from '../../apis';
 
 function ItemPropertyHosted (props) {
   // TODO: set button links
-  const { pid, title, ptype, nbed, nbath, thumb, price, reviews, published } = props;
+  const { token, pid, title, ptype, nbed, nbath, thumb, price, reviews, published } = props;
+
+  // state
+  const [dateShow, setDateShow] = useState(false);
+  const [dateInput, setDateInput] = useState([{ start: new Date(0), end: new Date(2099, 12, 31) }]);
+  const [alert, setAlert] = useState(false);
+  const [alertMsg, setAlertMsg] = useState('');
 
   // calculate avg rate
   let rateSun = 0;
   reviews.forEach(x => { rateSun += x.rate });
   const rateAvg = rateSun / reviews.length;
 
+  // private: add date range
+  function popDate (s, e) {
+    const tmp = dateInput;
+    tmp.push({ start: s, end: e });
+    setDateInput(tmp);
+  }
+
+  // private: update one of date range
+  function updateDateStart (idx, s) {
+    const tmp = dateInput;
+    tmp[idx].start = s;
+    setDateInput(tmp);
+  }
+
+  function updateDateEnd (idx, e) {
+    const tmp = dateInput;
+    tmp[idx].end = e;
+    setDateInput(tmp);
+  }
+
+  // private: publush
+  function pPublish () {
+    listPublish(token, pid, dateInput)
+      .then((res) => navigate('/dashboard'))
+      .catch((res) => {
+        setAlert(true);
+        setAlertMsg(res);
+      })
+  }
+
+  // private: unpublish
+  function pUnpublish () {
+    listUnpublish(token, pid)
+      .then((res) => navigate('/dashboard'))
+      .catch((res) => {
+        setAlert(true);
+        setAlertMsg(res);
+      })
+  }
+
+  // private: delete
+  function pRemove () {
+    listDelete(token, pid)
+      .then((res) => navigate('/dashboard'))
+      .catch((res) => {
+        setAlert(true);
+        setAlertMsg(res);
+      })
+  }
+
+  // private: multi-dateinput
+  function generateInputGroup () {
+    dateInput.map((x, idx) =>
+      <>
+        <Form.Group as={Row} className='mb-3'>
+          <Form.Label column sm='2'>From</Form.Label>
+          <Col sm='10'><Form.Control type='date' className='form-control' onChange={e => updateDateStart(idx, new Date(e.target.value))} value={x.start} /></Col>
+        </Form.Group>
+        <Form.Group as={Row} className='mb-3'>
+          <Form.Label column sm='2'>To</Form.Label>
+          <Col sm='10'><Form.Control type='date' className='form-control' onChange={e => updateDateEnd(idx, new Date(e.target.value))} value={x.end} /></Col>
+        </Form.Group>
+      </>
+    )
+  }
+
   return (
     <Card>
+      { alert && <Alert variant='danger' onClose={() => setAlert(false)} dismissible>{alertMsg}</Alert> }
       <Card.Img variant='top' src={thumb} />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
@@ -30,10 +110,14 @@ function ItemPropertyHosted (props) {
         <Button variant='primary'>Manage Booking</Button>
         {
           published
-            ? <Button variant='primary'>Unpublish</Button>
-            : <Button variant='primary'>Publish</Button>
+            ? <Button variant='primary' onClick={() => pUnpublish()}>Unpublish</Button>
+            : <Button variant='primary' onClick={() => setDateShow(true)}>Publish</Button>
         }
-        <Button variant='primary'>Remove</Button>
+        <Button variant='primary' onClick={() => pRemove()}>Remove</Button>
+        { dateShow && <h5>Book A Session</h5> }
+        { dateShow && generateInputGroup() }
+        { dateShow && <Button variant='primary' onClick={() => popDate(new Date(0), new Date(2099, 12, 31))}>Add Range</Button> }
+        { dateShow && <Button variant='primary' onClick={() => pPublish()}>Submit</Button> }
       </Card.Body>
     </Card>
   )
