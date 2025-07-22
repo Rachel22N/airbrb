4ca343543c94bfb5a36eba4871079585db5ed74f
Rachel(Liuqing) Yang <107014686+Rachel22N@users.noreply.github.com>
2025-07-23 00:39:56 +1000

    input fs to support Render 2

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,6 @@ import morgan from 'morgan';
 import { InputError, AccessError } from './error.js';
 // import swaggerDocument from '../swagger.json';
 // import swaggerDocument from '../swagger.json' assert { type: 'json' };
-import fs from 'fs';
 const swaggerDocument = JSON.parse(fs.readFileSync(new URL('../swagger.json', import.meta.url)));
 
 import {
